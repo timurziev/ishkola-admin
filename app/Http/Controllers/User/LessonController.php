3583536662619ab4b28479ef40e3525fc3ebd8bd
teslaAi <storona77@gmail.com>
2018-06-25 14:59:56 +0300

    lessons from mirafox

--- app/Http/Controllers/User/LessonController.php
+++ app/Http/Controllers/User/LessonController.php
@@ -17,9 +17,89 @@ class LessonController extends Controller
      */
     public function index()
     {
-        $lessons = Lesson::paginate(20);
+        $email = 'ykcontacts@gmail.com';
+        $service_url ="https://room.nohchalla.com/mira/service/v2/persons/byLogin/$email";
+        $res = $this->sendrequest($service_url,array(),"GET",0);
+        $id=$res['personid'];
+        $service_url ="https://room.nohchalla.com/mira/service/v2/myMeasures/".$id."/webinars";
+        $lessons = $this->sendrequest($service_url,array(),"GET",0);
 
-        return view('admin.lessons.index', ['lessons' => $lessons]);
+
+
+        return view('user.lessons.index', ['lessons' => $lessons]);
+    }
+
+    public function signit($url,$params)
+    {
+        $appid='system';//идентификатор приложения
+        $secretkey='yU7RFszU';//ключ системы
+        $ret_params=$params;
+//массив передаваемых параметров
+        ksort($ret_params);
+//сортировка параметров по названию
+        $ret_params['appid']=$appid;
+//помещение в конец массива параметра appid
+        $signstring="$url?";
+//формирование строки для подписи начиная с url
+        foreach($ret_params as $key=>$val)
+        {
+            if
+            (($val!="")||(gettype($val)!="string"))
+            {
+                $signstring.="$key=$val&";
+//добавление в строку для подписи очередного параметра
+            }
+        }
+        $signstring.="secretkey=$secretkey";
+//дополнение строки для подписи параметром secretkey
+        $ret_params['sign']=strtoupper(md5($signstring));
+//формирование ключа и добавление его в массив параметров
+        return $ret_params;
+    }
+
+    public function sendrequest($url,$parameters,$method, $ret_crange)
+    {
+//дополнение массива параметров значениями appid и sign (используется выше описанная функция signit)
+        $curl_data=$this->signit($url,$parameters);
+        $ch=curl_init();//инициализация дескриптора запроса
+        curl_setopt($ch,CURLOPT_ENCODING,'UTF-8');//заданиекодировкизапроса
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//возвратрезультата
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);//делает возможным переход на страницу ошибки
+        curl_setopt($ch,CURLOPT_HEADER,$ret_crange); //делаетвозможнымвозвращение заголовка Content-Range
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,$method);//заданиеметодазапроса
+        $query=http_build_query($curl_data);//построение строки параметров
+        switch($method)
+        {
+            case "PUT"://для PUT необходимо передавать длину строки параметров
+                curl_setopt($ch,CURLOPT_HTTPHEADER,array("Content-Length: ".strlen($query)));
+            case "POST"://параметры PUTи POST передаются в теле запроса
+                curl_setopt($ch,CURLOPT_POSTFIELDS,$query);
+                break;
+            case "GET"://для GET и DELETE параметры указываются в заголовке
+            case "DELETE":
+                $url.="?$query";
+        }
+        curl_setopt($ch,CURLOPT_URL,$url);//задание url запроса
+        $curl_response=curl_exec($ch);//выполнениезапроса
+        $response=json_decode($curl_response,true);//парсингрезультатов
+        if(!$response)
+            $response=$curl_response;//если результат не json
+        $code=curl_getinfo($ch,CURLINFO_HTTP_CODE); //получениекодарезультата
+        curl_close($ch);//анализответа
+        if($code!=200){
+            throw new Exception("НеправильныйHTTPкод: ".$code);
+        }
+        else
+            if
+            (
+                is_array($response)&&isset($response["errorMessage"]))
+            {
+                throw new Exception("Возвращенаошибка: ".$response["errorMessage"]);
+            }
+            else
+            {
+                return $response;
+            }
     }
 
     /**
---
+++ resources/views/user/lessons/index.blade.php
@@ -0,0 +1,44 @@
+@extends('user.layouts.user')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Занятия</h2>
+                    <ul class="nav navbar-right panel_toolbox" style="min-width: 0;">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0"
+                               width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Информация</th>
+                                    <th>Дата начала</th>
+                                    <th>Дата окончания</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($lessons as $lesson)
+                                    <tr>
+                                        <td><a href="{{ $lesson['link'] }}">{{ $lesson['mename'] }}</a></td>
+                                        <td>{{ $lesson['mestartdate'] }}</td>
+                                        <td>{{ $lesson['meenddate'] }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    {{--<div class="pull-right">--}}
+                        {{--{{ $lessons->links() }}--}}
+                    {{--</div>--}}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
